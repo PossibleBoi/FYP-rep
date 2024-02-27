f1ba5d672a06eccca25e9460fd2f29f614281257
PossibleBoi <100285488+PossibleBoi@users.noreply.github.com>
2024-02-27 14:52:06 +0545

    From laptop changes
    Change in navigation for roles

---
+++ reactjs/src/navbar/navigation.jsx
@@ -0,0 +1,21 @@
+import AuthUser from '../components/AuthUser';
+import AdminNav from './admin';
+import Guest from './guest';
+import UserNav from './user';
+
+
+export default function Auth() {
+    if (AuthUser().user.role == 'admin') {
+        return <AdminNav />
+    }
+    else if (AuthUser().user.role == 'user') {
+        return <UserNav />
+    }
+    else if(AuthUser().user.role == 'starter'){
+        return 
+    }
+    else
+    {
+        return <Guest />
+    }
+}
